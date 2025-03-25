85a9fa44d7cdb978f0a7b4e96ebbd0ba439660d0
EPham42747 <ethancpham@gmail.com>
2025-03-25 11:12:24 -0400

    Added routes

---
+++ src/app/api/add/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  
+  return NextResponse.json(
+    { id: id },
+    { status: 200 }
+  );
+};
