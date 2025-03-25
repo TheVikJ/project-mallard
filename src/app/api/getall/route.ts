e195048f2a6bdc6cc287c9c06d52e363f064a885
EPham42747 <ethancpham@gmail.com>
2025-03-25 12:05:51 -0400

    Created /getall endpoint

--- src/app/api/getall/route.ts
+++ src/app/api/getall/route.ts
@@ -1,8 +1,14 @@
 import { NextResponse } from 'next/server';
 
+import prisma from '@/utils/prisma';
+
 export async function GET() {
+  const notifs = await prisma.notifications.findMany({
+    include: { PolicyNotif: true, ClaimNotif: true, NewsNotif: true },
+  });
+  
   return NextResponse.json(
-    {},
-    { status: 200 }
+    notifs,
+    { status: notifs.length > 0 ? 200 : 404 }
   );
 };
