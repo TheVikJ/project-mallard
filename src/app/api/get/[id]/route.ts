da27c8320140ee329c7938f2fb19e353096c291c
EPham42747 <ethancpham@gmail.com>
2025-03-31 16:57:15 -0400

    Updated getall to require userId

--- src/app/api/get/[id]/route.ts
+++ src/app/api/get/[id]/route.ts
@@ -3,10 +3,10 @@ import prisma from '@/utils/prisma';
 
 export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-  const userId = parseInt(id);
+  const notifId = parseInt(id);
   
   const notif = await prisma.notifications.findUnique({
-    where: { id: userId },
+    where: { id: notifId },
     include: { PolicyNotif: true, ClaimNotif: true, NewsNotif: true },
   });
   
--- src/app/api/getall/route.ts
+++ src/app/api/getall/route.ts
@@ -1,9 +1,16 @@
-import { NextResponse } from 'next/server';
-
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/utils/prisma';
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const request = await req.json();
+  const userId = request.body.userId;
+  if (!userId) return NextResponse.json(
+    { message: 'User ID must be supplied' },
+    { status: 400 }
+  );
+
   const notifs = await prisma.notifications.findMany({
+    where: { sender:  },
     include: { PolicyNotif: true, ClaimNotif: true, NewsNotif: true },
   });
   
